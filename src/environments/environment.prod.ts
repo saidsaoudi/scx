b92b62884f6133f33826238ece77a1493b684f1b
anas-senhaji <senhajianas0@gmail.com>
2025-03-01 14:35:45 +0000

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
     // URL: 'http://localhost:8001/api/',
   // STORAGE: 'http://localhost:8001/storage/',
-  URL: 'https://stag.api.visiostation.com/v1/',
-  STORAGE: 'http://localhost:8001/storage/',
+  URL: 'http://localhost:8000/api/',
+  STORAGE: 'http://localhost:8000/storage/',
 };
