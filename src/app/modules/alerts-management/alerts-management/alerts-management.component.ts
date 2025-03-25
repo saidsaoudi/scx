484bc6cc714a055393b637a23a353e9ca82974d8
saidsaoudi <said.iduoas@gmail.com>
2025-03-25 11:39:07 +0000

    alert management

--- src/app/modules/alerts-management/alerts-management/alerts-management.component.ts
+++ src/app/modules/alerts-management/alerts-management/alerts-management.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Region {
+  name: string;
+  code: string;
+}
+interface Province {
+  name: string;
+  code: string;
+}
+interface Hopital {
+  name: string;
+  code: string;
+}
+interface AutoCompleteCompleteEvent {
+  originalEvent: Event;
+  query: string;
+}
 @Component({
   selector: 'app-alerts-management',
   standalone : false,
@@ -8,4 +24,36 @@ import { Component } from '@angular/core';
 })
 export class AlertsManagementComponent {
 
+  headerColumuns = [
+    'Criticité',
+    'Type alerte',
+    'Produit',
+    'Classe',
+    'DCI',
+    'Lieu',
+    'Date et heure',
+    'Status'  
+  ];
+
+  regions: Region[] | undefined;
+  selectedRegion: Region | undefined;
+
+  province : Province[] | undefined;
+  selectedProvince: Province | undefined;
+
+  hopital : Hopital[] | undefined;
+  selectedHopital: Hopital | undefined;
+  rangeDates: Date[] | undefined;
+
+  items: any[] | undefined;
+
+  selectedItem: any;
+
+  suggestions: any[] ;
+  date1: Date;
+  date2: Date;
+  toutes: string[] = [];
+  search(event: AutoCompleteCompleteEvent) {
+      this.suggestions = [...Array(10).keys()].map(item => event.query + '-' + item);
+  }
 }
